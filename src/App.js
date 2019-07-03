c5f28bd041f4f6b5361e815aad98bfe6a68d5ed3
Student <jsitbon@qcc.cuny.edu>
2019-07-03 16:36:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -10,14 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        
         >
           Learn React
         </a>
+
+        <p> This is <code>another code.</code></p>
       </header>
     </div>
   );
